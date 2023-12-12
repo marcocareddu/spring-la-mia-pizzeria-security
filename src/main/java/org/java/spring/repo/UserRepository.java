40f6785c7184792f0fd13e97141a1338bed462b1
marcocareddu <saldumaccu@tuta.io>
2023-12-12 12:10:19 +0100

    Fixed UserRepository

--- src/main/java/org/java/spring/repo/UserRepository.java
+++ src/main/java/org/java/spring/repo/UserRepository.java
@@ -1,10 +1,10 @@
 package org.java.spring.repo;
 
-import org.java.spring.Ingredient;
+import org.java.spring.auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Ingredient, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer>{
 
 }
